e5031b9d0174aa6ab3003258a27e8d6ad1a83ac0
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-09 00:43:33 +0530

    author service interface update

--- src/main/java/com/PersonalProject/library/service/AuthorService.java
+++ src/main/java/com/PersonalProject/library/service/AuthorService.java
@@ -1,9 +1,17 @@
 package com.PersonalProject.library.service;
 
+import com.PersonalProject.library.dto.AuthorDTO;
 import com.PersonalProject.library.dto.AuthorSaveDTO;
+import com.PersonalProject.library.dto.AuthorUpdateDTO;
+
+import java.util.List;
 
 public interface AuthorService {
 
 
     String addAuthor(AuthorSaveDTO authorsaveDTO);
+
+    List<AuthorDTO> getAllAuthor();
+
+    String updateAuthor(AuthorUpdateDTO authorUpdateDTO);
 }
