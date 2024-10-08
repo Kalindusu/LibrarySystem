86b98ec055017bae0435414ba6cb26498a265ff9
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-09 00:44:20 +0530

    authorConntroller file creation

--- src/main/java/com/PersonalProject/library/Controller/AuthorController.java
+++ src/main/java/com/PersonalProject/library/Controller/AuthorController.java
@@ -1,12 +1,14 @@
 package com.PersonalProject.library.Controller;
 
+import com.PersonalProject.library.dto.AuthorDTO;
 import com.PersonalProject.library.dto.AuthorSaveDTO;
+import com.PersonalProject.library.dto.AuthorUpdateDTO;
 import com.PersonalProject.library.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -14,10 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthorController {
     @Autowired
     private AuthorService authorService;
-
+@PostMapping(path="/save")
     public String saveAuthor(@RequestBody AuthorSaveDTO authorsaveDTO)
     {
         String   authorname=authorService.addAuthor(authorsaveDTO);
+        return  "Added Successfully";
+    }
+
+    @GetMapping(path="/getAllAuthor")
+    public List<AuthorDTO> getAllAuthor()
+    {
+        List<AuthorDTO> allAuthors=authorService.getAllAuthor();
+        return allAuthors;
+    }
+
+    @PutMapping(path="/update")
+
+    public String getAllAuthor(@RequestBody AuthorUpdateDTO authorUpdateDTO)
+
+    {
+        String authorname=authorService.updateAuthor(authorUpdateDTO);
         return  authorname;
     }
 
