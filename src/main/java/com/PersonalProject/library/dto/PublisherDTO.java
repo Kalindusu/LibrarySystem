da480e91545aca07f4dba6cfa1eaace0252693c3
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-11 01:06:49 +0530

    publisher DTO file

---
+++ src/main/java/com/PersonalProject/library/dto/PublisherDTO.java
@@ -0,0 +1,17 @@
+package com.PersonalProject.library.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PublisherDTO {
+        int publisher_id;
+        private String name;
+}
