4c86cd7b78ef6ceb1a7f46cda94487d1a849ccb8
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-11 01:05:58 +0530

    publisher DTO file

---
+++ src/main/java/com/PersonalProject/library/dto/PublisherUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.PersonalProject.library.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PublisherUpdateDTO {
+
+    int publisher_id;
+    private String name;
+}
