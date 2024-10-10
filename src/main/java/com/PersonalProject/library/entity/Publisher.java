0bc3bc9b4ba8fefe6d0eefefdfafdf44e5b1a550
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-11 00:53:30 +0530

    publisher file create and add implementation

---
+++ src/main/java/com/PersonalProject/library/entity/Publisher.java
@@ -0,0 +1,37 @@
+package com.PersonalProject.library.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="publisher")
+public class Publisher {
+@Id
+@GeneratedValue(strategy= GenerationType.AUTO)
+@Column(name="Publisher_id")
+    private int publisher_id;
+@Column(name="Publisher_name")
+    private String name;
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    public Publisher() {
+    }
+
+    public int getPublisher_id() {
+        return publisher_id;
+    }
+
+    public void setPublisher_id(int publisher_id) {
+        this.publisher_id = publisher_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
