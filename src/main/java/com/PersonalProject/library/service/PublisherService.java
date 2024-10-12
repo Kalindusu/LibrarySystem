772d34d3f63333352a3f6b4c0dace9feeac1fcfc
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-12 23:19:56 +0530

    PublisherService create

--- src/main/java/com/PersonalProject/library/service/PublisherService.java
+++ src/main/java/com/PersonalProject/library/service/PublisherService.java
@@ -1,7 +1,12 @@
 package com.PersonalProject.library.service;
 
+import com.PersonalProject.library.dto.AuthorDTO;
 import com.PersonalProject.library.dto.PublisherSaveDTO;
+
+import java.util.List;
 
 public interface PublisherService {
     String addPublisher(PublisherSaveDTO publisherSaveDTO);
+
+    List<AuthorDTO> getAllPublisher();
 }
