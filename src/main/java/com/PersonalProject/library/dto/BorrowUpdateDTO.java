e49964d3724f6c5cb1bfed3f07a09a820b43865f
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-10-23 19:37:02 +0530

    BorrowUpdateDTO create

---
+++ src/main/java/com/PersonalProject/library/dto/BorrowUpdateDTO.java
@@ -0,0 +1,19 @@
+package com.PersonalProject.library.dto;
+
+import com.PersonalProject.library.entity.Book;
+import com.PersonalProject.library.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BorrowUpdateDTO {
+    private int id;
+    private int book_id;
+    private int user_id;
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
+}
