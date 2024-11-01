b77d952f5927af9d68e4f8c1f11ec4397f85e8bb
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-11-01 10:45:28 +0530

    UserSaveDTO  create

---
+++ src/main/java/com/PersonalProject/library/dto/UserSaveDTO.java
@@ -0,0 +1,15 @@
+package com.PersonalProject.library.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserSaveDTO {
+    private String userid;
+    private  String name;
+    private String email;
+}
